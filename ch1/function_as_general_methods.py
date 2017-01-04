5b36440241f2ed3a49b0d55a76809bde23baab39
LittleStupid <lttlstpd@live.com>
2017-01-04 20:10:30 +0800

    add square_root function

--- ch1/function_as_general_methods.py
+++ ch1/function_as_general_methods.py
@@ -1,6 +1,14 @@
 def main():
     print(iter_improve(gold_update, gold_test))
     print((1 + pow(5, 1 / 2)) / 2)
+    print('-------')
+    print(square_root(2))
+    print(square_root(3))
+    print(square_root(9))
+
+
+def average(x, y):
+    return (x + y) / 2
 
 
 def square(x):
@@ -25,6 +33,7 @@ def near(x, f, g):
     return approx_eq(f(x), g(x))
 
 
+# -----------------------
 def gold_update(guess):
     return 1 / guess + 1
 
@@ -32,6 +41,17 @@ def gold_update(guess):
 # solve the equation x^2-x-1=0
 def gold_test(guess):
     return near(guess, square, increment)
+
+
+# -----------------------
+def square_root(x):
+    def update(g):
+        return average(g, x / g)
+
+    def test(g):
+        return approx_eq(x, square(g))
+
+    return iter_improve(update, test)
 
 if __name__ == "__main__":
     main()
