1ab3fa671e890806036e50eb7ce2173f27b6b9bb
LittleStupid <lttlstpd@live.com>
2017-01-04 19:59:50 +0800

    function as general methods(done)

--- ch1/function_as_general_methods.py
+++ ch1/function_as_general_methods.py
@@ -1,11 +1,37 @@
 def main():
-    print('this is main')
+    print(iter_improve(gold_update, gold_test))
+    print((1 + pow(5, 1 / 2)) / 2)
+
+
+def square(x):
+    return x * x
+
+
+def increment(x):
+    return x + 1
 
 
 def iter_improve(update, isclose, guess=1):
     while not isclose(guess):
         guess = update(guess)
     return guess
+
+
+def approx_eq(x, y, tolerance=1e-5):
+    return abs(x - y) < tolerance
+
+
+def near(x, f, g):
+    return approx_eq(f(x), g(x))
+
+
+def gold_update(guess):
+    return 1 / guess + 1
+
+
+# solve the equation x^2-x-1=0
+def gold_test(guess):
+    return near(guess, square, increment)
 
 if __name__ == "__main__":
     main()
