6b0c870dee24161ad4c5c2fff345fccd5df9912d
LittleStupid <lttlstpd@live.com>
2017-01-05 12:43:03 +0800

    update tuple_list with message dispath

--- ch2/tuple_list.py
+++ ch2/tuple_list.py
@@ -26,13 +26,43 @@ def getitem_rlist(s, i):
     return first(s)
 
 
+def make_mutable_rlist():
+    contents = empty_rlist
+
+    def dispatch(message, value=None):
+        nonlocal contents
+        if message == 'len':
+            return len_rlist(contents)
+        elif message == 'getitem':
+            return getitem_rlist(contents, value)
+        elif message == 'push_first':
+            contents = make_rlist(value, contents)
+        elif message == 'pop_first':
+            f = first(contents)
+            contents = rest(contents)
+            return f
+        elif message == 'str':
+            return str(contents)
+    return dispatch
+
+
 def main():
+    print('----------')
     counts = make_rlist(1, make_rlist(
         2, make_rlist(3, make_rlist(4, empty_rlist))))
     print(first(counts))
     print(rest(counts))
     print(len_rlist(counts))
     print(getitem_rlist(counts, 2))
+    print('----------')
+    another_list = make_mutable_rlist()
+    print(another_list('len'))
+    another_list('push_first', 1)
+    another_list('push_first', 2)
+    another_list('push_first', 3)
+    print(another_list('str'))
+    print(another_list('len'))
+    print(another_list('getitem', 2))
 
 if __name__ == "__main__":
     main()
