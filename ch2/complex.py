046e0d498f1eb7dc120e90f17daca14c21abe26f
LittleStupid <lttlstpd@live.com>
2017-01-05 21:11:19 +0800

    update complex_ration

--- ch2/complex.py
+++ ch2/complex.py
@@ -1,4 +1,7 @@
 from math import atan2, sin, cos, pi
+from fractions import gcd
+
+# ---------------------------------------------------
 
 
 def add_complex(z1, z2):
@@ -55,6 +58,87 @@ class ComplexRI(object):
 
     def __repr__(self):
         return 'ComplexRI({0}, {1})'.format(self.real, self.image)
+# ---------------------------------------------------
+
+
+class Rational(object):
+
+    def __init__(self, number, denom):
+        g = gcd(number, denom)
+        self.number = number
+        self.denom = denom
+
+    def __repr__(self):
+        return 'Rational({0}, {1})'.format(self.number, self.denom)
+
+
+def add_rational(x, y):
+    nx, dx = x.number, x.denom
+    ny, dy = y.number, y.denom
+    return Rational(nx * dy + ny * dx, dx * dy)
+
+
+def mul_rational(x, y):
+    return Rational(x.number * y.number, x.denom * y.denom)
+
+
+def iscomplex(z):
+    return type(z) in (ComplexRI, ComplexMA)
+
+
+def isrational(z):
+    return type(z) == Rational
+
+# ---------------------------------------------------
+
+
+def add_complex_and_rational(z, r):
+    return ComplexRI(z.real + r.number / r.denom, z.image)
+
+
+def add_mix(z1, z2):
+    if iscomplex(z1) and iscomplex(z2):
+        return add_complex(z1, z2)
+    elif iscomplex(z1) and isrational(z2):
+        return add_complex_and_rational(z1, z2)
+    elif isrational(z1) and iscomplex(z2):
+        return add_complex_and_rational(z2, z1)
+    else:
+        return add_rational(z1, z2)
+# ---------------------------------------------------
+
+
+def add(z1, z2):
+    types = (type_tag(z1), type_tag(z2))
+    return add.imp[types](z1, z2)
+
+add.imp = {}
+add.imp[('com', 'com')] = add_complex
+add.imp[('com', 'rat')] = add_complex_and_rational
+add.imp[('rat', 'com')] = lambda x, y: add_complex_and_rational(y, x)
+add.imp[('rat', 'rat')] = add_rational
+
+
+def type_tag(x):
+    return type_tag.tags[type(x)]
+
+type_tag.tags = {ComplexRI: 'com', ComplexMA: 'com', Rational: 'rat'}
+
+# ---------------------------------------------------
+
+
+# def ration_to_complex(x):
+#     return ComplexRI(x.number / x.denom, 0)
+#
+# coercions = {('rat', 'com'): ration_to_complex}
+#
+#
+# def coerce_add(x, y):
+#     tx, ty = type_tag(x), type_tag(y)
+#     if tx != ty:
+#         if(tx,ty)
+
+# ---------------------------------------------------
 
 
 def main():
@@ -64,6 +148,23 @@ def main():
     print(mul_complex(ComplexRI(0, 1), ComplexRI(0, 1)))
     print(ComplexRI(0, 1) * ComplexRI(0, 1))
 
+    print('--------------------------------------------')
+
+    c1 = ComplexRI(1, 2)
+    c2 = ComplexRI(2, 2)
+    r1 = Rational(1, 3)
+    r2 = Rational(2, 5)
+
+    print(add_mix(c1, c2))
+    print(add_mix(c1, r1))
+    print(add_mix(r1, c1))
+    print(add_mix(r1, r2))
+
+    print(add(c1, c2))
+    print(add(c1, r1))
+    print(add(r1, c1))
+    print(add(r1, r2))
+    print('--------------------------------------------')
 
 if __name__ == "__main__":
     main()
