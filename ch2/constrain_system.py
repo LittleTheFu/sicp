d5223d2f2e633f91e0458b5c2c7d4d16a986ed13
LittleStupid <lttlstpd@live.com>
2017-01-05 15:37:27 +0800

    add constrain system

---
+++ ch2/constrain_system.py
@@ -0,0 +1,94 @@
+from operator import add, sub, mul, truediv
+
+
+def make_ternary_constraint(a, b, c, ab, bc, ca):
+    def new_value():
+        av, bv, cv = [connector['has_val']() for connector in (a, b, c)]
+        if av and bv:
+            c['set_val'](constraint, ab(a['val'], b['val']))
+        elif av and cv:
+            b['set_val'](constraint, ca(c['val'], a['val']))
+        elif bc and cv:
+            a['set_val'](constraint, bc(b['val'], c['val']))
+
+    def forget_value():
+        for connector in (a, b, c):
+            connector['forget'](constraint)
+    constraint = {'new_val': new_value, 'forget': forget_value}
+    for connector in (a, b, c):
+        connector['connect'](constraint)
+    return constraint
+
+
+def adder(a, b, c):
+    return make_ternary_constraint(a, b, c, add, sub, sub)
+
+
+def multiplier(a, b, c):
+    return make_ternary_constraint(a, b, c, mul, truediv, truediv)
+
+
+def constant(connector, value):
+    constraint = {}
+    connector['set_val'](constraint, value)
+    return constraint
+
+
+def make_connector(name=None):
+    informant = None
+    constraints = []
+
+    def set_value(source, value):
+        nonlocal informant
+        val = connector['val']
+        if val is None:
+            informant, connector['val'] = source, value
+            if name is not None:
+                print(name, '=', value)
+            inform_all_except(source, 'new_val', constraints)
+        else:
+            if val != value:
+                print('Contridiction detected:', val, 'vs', value)
+
+    def forget_value(source):
+        nonlocal informant
+        if informant == source:
+            informant, connector['val'] = None, None
+            if name is not None:
+                print(name, 'is forgetten')
+            inform_all_except(source, 'forget', constraints)
+
+    connector = {'val': None, 'set_val': set_value, 'forget': forget_value,
+                 'has_val': lambda: connector['val'] is not None,
+                 'connect': lambda source: constraints.append(source)}
+    return connector
+
+
+def inform_all_except(source, message, constrains):
+    for c in constrains:
+        if c != source:
+            c[message]()
+
+
+def make_converter(c, f):
+    u, v, w, x, y = [make_connector() for _ in range(5)]
+    multiplier(c, w, u)
+    multiplier(v, x, u)
+    adder(v, y, f)
+    constant(w, 9)
+    constant(x, 5)
+    constant(y, 32)
+
+
+def main():
+    celsius = make_connector('Celsius')
+    fahreheit = make_connector('Fahreheit')
+    make_converter(celsius, fahreheit)
+
+    # fahreheit['set_val']('usr', 212)
+    # fahreheit['forget']('usr')
+    celsius['set_val']('usr', 25)
+
+
+if __name__ == "__main__":
+    main()
