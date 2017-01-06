4b3ffb74867a49fce5ec1c361e73a2add2136333
LittleStupid <lttlstpd@live.com>
2017-01-06 13:26:28 +0800

    add tree

---
+++ ch3/tree.py
@@ -0,0 +1,29 @@
+class Tree(object):
+
+    def __init__(self, entry, left=None, right=None):
+        self.entry = entry
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        args = repr(self.entry)
+        if self.left or self.right:
+            args += ', {0}, {1}'.format(repr(self.left), repr(self.right))
+        return 'Tree({0})'.format(args)
+
+
+def fib_tree(n):
+    if n == 1:
+        return Tree(0)
+    if n == 2:
+        return Tree(1)
+    left = fib_tree(n - 2)
+    right = fib_tree(n - 1)
+    return Tree(left.entry + right.entry, left, right)
+
+
+def main():
+    print(fib_tree(5))
+
+if __name__ == "__main__":
+    main()
